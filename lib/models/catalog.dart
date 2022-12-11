c928b9afb9d3728e7f2749cededb5fa07dafeb85
Aditya Pandya <pandyaaditya901@gmail.com>
2022-12-11 19:16:15 +0530

    day11 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+  final products = [Item(
+    id: 1,
+    name: "iPhone 12 Pro",
+    desc: "Apple iPhone 12th generation",
+    price : 999,
+    color: "33505a",
+    image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000";
+  )];
